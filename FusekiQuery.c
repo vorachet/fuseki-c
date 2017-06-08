bef9e470efd6f6341388b6acd7d53fdc93301d2e
Vorachet Jaroensawas <vorachet@gmail.com>
2017-06-08 14:35:59 +0700

    init

---
+++ FusekiQuery.c
@@ -0,0 +1,76 @@
+//
+//  FusekiQuery.c
+//
+//  Created by Vorachet Jaroensawas (vorachet@gmail.com) on 6/8/17.
+//  MIT License
+//  Copyright © 2017 vorachet. All rights reserved.
+//
+// XML  Accept: application/sparql-results+xml  (as specified by https://www.w3.org/TR/rdf-sparql-XMLres/)
+// JSON Accept: application/sparql-results+json (as specifed by https://www.w3.org/TR/sparql11-results-json/)
+// TSV  Accept: text/tab-separated-values (as specified by https://www.w3.org/TR/sparql11-results-csv-tsv/)
+// CSV  Accept: text/csv  (as specified by https://www.w3.org/TR/sparql11-results-csv-tsv/)
+
+// Compile: gcc -Wall -o FusekiQuery  FusekiQuery.c  -lcurl
+
+// Usage:
+//   FusekiQuery  http://localhost:3030/ds/sparql  application/sparql-results+json   "select * where {?s ?p ?o}"
+//   FusekiQuery  http://localhost:3030/ds/sparql  application/xml  "select * where {?s ?p ?o}"
+//   FusekiQuery  http://localhost:3030/ds/sparql  text/csv  "select * where {?s ?p ?o}"
+//   FusekiQuery  http://localhost:3030/ds/sparql  text/tab-separated-values  "select * where {?s ?p ?o}"
+
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+
+int main(int argc, char **argv)
+{
+    if (argc != 4) {
+        printf("usage: ./FusekiQuery http://localhost:3030/ds/sparql application/sparql-results+json \"select * where {?s ?p ?o}\"\n");
+        return -1;
+    }
+
+    char *url = argv[1];
+    char *headerAccept = argv[2];
+    char *sparql = argv[3];
+
+    CURL *curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+    if(curl) {
+
+        struct curl_slist *chunk = NULL;
+
+        char httpHeaderAccept[2000];
+        strcat (httpHeaderAccept, "Accept: ");
+        strcat (httpHeaderAccept, headerAccept);
+        printf("httpHeaderAccept = %s\n", httpHeaderAccept);
+
+
+        chunk = curl_slist_append(chunk, "Accept: application/sparql-results+json");
+        chunk = curl_slist_append(chunk, "ContentType: application/sparql-query");
+
+        char queryParam[2000];
+        strcat (queryParam, "query=");
+        strcat (queryParam, sparql);
+        printf("queryParam = %s\n", queryParam);
+
+        res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, queryParam);
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                    curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+        curl_slist_free_all(chunk);
+    }
+
+    curl_global_cleanup();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # fuseki-c
-Utility C programs for Jena Fuseki  
+Utility C programs for Jena Fuseki
+
+## References
+
+```
+XML  Accept: application/sparql-results+xml  (as specified by https://www.w3.org/TR/rdf-sparql-XMLres/)
+JSON Accept: application/sparql-results+json (as specifed by https://www.w3.org/TR/sparql11-results-json/)
+TSV  Accept: text/tab-separated-values (as specified by https://www.w3.org/TR/sparql11-results-csv-tsv/)
+CSV  Accept: text/csv  (as specified by https://www.w3.org/TR/sparql11-results-csv-tsv/)
+```
+
+## FusekiQuery
+
+### Compile
+
+libcurl is required
+```
+gcc -Wall -o FusekiQuery  FusekiQuery.c  -lcurl
+```
+
+### Run
+
+./FusekiQuery  http://localhost:3030/ds/sparql  application/sparql-results+json   "select * where {?s ?p ?o}"
+./FusekiQuery  http://localhost:3030/ds/sparql  application/xml  "select * where {?s ?p ?o}"
+./FusekiQuery  http://localhost:3030/ds/sparql  text/csv  "select * where {?s ?p ?o}"
+./FusekiQuery  http://localhost:3030/ds/sparql  text/tab-separated-values  "select * where {?s ?p ?o}"
+```
